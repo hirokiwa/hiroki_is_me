a47386c6b2191397073c5dbe60f1f5d0e85992e0
hirokiwa <hirokifsys@gmail.com>
2022-09-15 14:46:39 +0900

    now developing

---
+++ src/page/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+const Home = ():JSX.Element => {
+  return (
+    <div className='Home'>
+      <h1>ひろきの自己紹介サイト</h1>
+      <div className="loader">Loading...</div>
+      <div>開発中</div>
+      <div>当日までに完成させます。</div>
+    </div>
+  );
+}
+
+export default Home;
