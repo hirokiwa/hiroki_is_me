89eff0b4bf628eb9a236eb493c76a7c9f99faf64
hirokiwa <hirokifsys@gmail.com>
2022-09-17 00:29:09 +0900

    thank for living

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -6,7 +6,10 @@ const Home = ():JSX.Element => {
   return (
     <div className='Home'>
       <Header/>
-      <img className='top_img' src="./top_img.jpg" alt="icon" />
+      <div className='top_img_outer'>
+        <img className='top_img' src="https://raw.githubusercontent.com/hirokiwa/hiroki_is_me/develop/src/page/home/top_img.jpg" alt="icon" />
+        <div className='thank'>生きてるだけでありがとう</div>      
+      </div>
     </div>
   );
 }
