1dd01855b24d35b67de93b24fa9ec00cce0605c2
hirokiwa <hirokifsys@gmail.com>
2022-09-17 01:27:29 +0900

    trim

--- src/page/home/plof_card/PlofCard.tsx
+++ src/page/home/plof_card/PlofCard.tsx
@@ -4,7 +4,12 @@ import './PlofCard.css';
 const PlofCard = ():JSX.Element => {
   return (
     <div className='PlofCard'>
-        
+        <img className='me' src="https://raw.githubusercontent.com/hirokiwa/hiroki_is_me/develop/src/page/home/plof_card/VYHB9671.PNG" alt="hiroki" />
+        <div className='my_data'>
+            <h3 className='my_name'>ひろき</h3>
+            <div>2002年5月4日生</div>
+            <div>立命館大学理工学部電子情報工学科2年</div>
+        </div>
     </div>
   );
 }
