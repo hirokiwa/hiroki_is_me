dcad4f977a098162a98427e25bc6a78f9a5ec2c5
hirokiwa <hirokifsys@gmail.com>
2022-12-28 03:39:04 +0900

    set gradeStatus

---
+++ src/grade.ts
@@ -0,0 +1,3 @@
+export type grade = "1年" | "2年" | "3年" | "4年";
+
+export type gradeStates = grade | "卒";
--- src/parts/footer/Footer.tsx
+++ src/parts/footer/Footer.tsx
@@ -4,7 +4,11 @@ import './Footer.css';
 
 const Footer = ():JSX.Element => {
   return (
-    <div className='Footer'>
+      <div className='Footer'>
+        <div className='Footer_text'>
+            エンジニアリングに心を躍らせています。
+        </div>
+        <div className='footer_title'>Hiroki is me</div>  
         <Copyright></Copyright>
     </div>
   );
