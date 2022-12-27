f8637bcba547656dcfaeb717f5d1ce57bb142a04
hirokiwa <hirokifsys@gmail.com>
2022-12-28 04:38:17 +0900

    add pc mode

--- src/page/home/introduction/Introduction.tsx
+++ src/page/home/introduction/Introduction.tsx
@@ -5,7 +5,9 @@ import '../../../common_style/Card.css'
 const Introduction = ():JSX.Element => {
   return (
     <div className='Introduction Card'>
-      <h3 className='card_title'>ごあいさつ</h3>
+      <div className='card_title_outer'>
+        <h3 className='card_title'>ごあいさつ</h3>
+      </div>
       <div className='main_text'>
         <div className='text'>鳥取県鳥取市に生まれ、砂丘のふもとで育ちました。昨日に追われ明日を追う、そんな毎日を過ごしております。</div>
       </div>
