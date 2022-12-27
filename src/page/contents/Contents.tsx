708b5bd8188d1197fabe2f727be36bfd6259fcaa
hirokiwa <hirokifsys@gmail.com>
2022-12-28 04:12:48 +0900

    add alexture diary card

--- src/page/contents/Contents.tsx
+++ src/page/contents/Contents.tsx
@@ -8,18 +8,22 @@ import data from './contents.json'
 import Copyright from '../../parts/copyright/Copyright';
 import { Link } from "react-router-dom";
 import '../../common_style/Link.css'
+import Footer from '../../parts/footer/Footer';
 
 const Contents = ():JSX.Element => {
   return (
     <div className='Contents'>
-        <Header/>
-        <div className='page_title'>コンテンツ</div>
-        <Repeat numTimes={data.length} display_flex = {false}>
-            {(index:number) => <ContentsCard data = {data[index]}/>}
-        </Repeat>
-        <Link className='back_page' to={`/`}>ホーム＜</Link>
-      <ButtonTop/>
-      <Copyright/>
+      <Header/>
+      <div className='Contents_inner'>
+          <div className='page_title'>コンテンツ</div>
+          <Repeat numTimes={data.length} display_flex = {false}>
+              {(index:number) => <ContentsCard data = {data[index]}/>}
+          </Repeat>
+          <Link className='back_page' to={`/`}>ホーム＜</Link>
+        <ButtonTop/>
+        <Copyright />
+      </div>
+      <Footer/>
     </div>
   );
 }
