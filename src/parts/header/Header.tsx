e9e5c50b3f94cdbf1d2b1d507e9779907439615c
hirokiwa <hirokifsys@gmail.com>
2023-01-01 23:12:43 +0900

    add titleLogo button

--- src/parts/header/Header.tsx
+++ src/parts/header/Header.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
 import './Header.css';
-
-const Header = ():JSX.Element => {
+const Header = (): JSX.Element => {
+  const clickTitle = () => {
+    window.location.pathname === "/" ?
+      setTimeout(() => { window.location.reload() }, 0):
+      window.location.href = "/";
+  }
   return (
     <div className='Header'>
+      <button className='HeaderInner' onClick={()=>{clickTitle()}}>
         <img className='headder_icon' src="https://hirokiwa.github.io/mypage/img/messageImage_1644108394365.jpg" alt="icon" />
-        <div className='title'>Hiroki is me</div>
+        <div className='title'>Hiroki is me</div> 
+      </button>
     </div>
   );
 }
